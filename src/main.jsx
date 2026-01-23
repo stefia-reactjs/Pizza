40f5d5684940b3087c1e37715756b358dcb84983
stefia <stefa.dxf@gmail.com>
2026-01-23 23:15:28 +0500

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Sneakers">
     <Provider store={store}>
       <App />
     </Provider>

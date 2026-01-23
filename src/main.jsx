05a8687438d8c0c11b8a936840a7e970160d7404
stefia <stefa.dxf@gmail.com>
2026-01-23 22:32:56 +0500

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router'
+import { BrowserRouter } from 'react-router'
 import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>,
+  </BrowserRouter>,
 )

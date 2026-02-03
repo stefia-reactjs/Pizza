b4ad7ab37ca0fa37370d194b94dcdeca8cbec4e3
stefia <stefa.dxf@gmail.com>
2026-02-03 20:11:57 +0500

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/Pizzas">
+  <BrowserRouter basename="/Pizza">
     <Provider store={store}>
       <App />
     </Provider>
